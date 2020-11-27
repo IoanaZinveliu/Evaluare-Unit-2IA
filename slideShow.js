79073d9a6a030f7e24247656c74d7e9243b4be30
IoanaZinveliu <73156295+IoanaZinveliu@users.noreply.github.com>
2020-11-27 18:45:29 +0200

    Add files via upload

---
+++ slideShow.js
@@ -0,0 +1,34 @@
+var slideIndex = 0;
+//showSlides();
+
+function show() {
+  var i;
+  var slides=document.getElementsByClassName("mySlides");
+ 
+ 
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  
+slides[slideIndex-1].style.display="block";
+	  
+  setTimeout(show, 3000); // Change image every  seconds
+}
+
+
+/*var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}*/
